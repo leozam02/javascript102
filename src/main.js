cb810e0091bceb79c7f67eebabcb1fa509c2ccf1
Leonardo Zambrano <151984141+leozam02@users.noreply.github.com>
2025-06-25 19:39:51 +0000

    Last change

--- src/main.js
+++ src/main.js
@@ -23,3 +23,62 @@ const itemData = {
         score: 36
     }
 };
+
+(() => {
+    document.addEventListener('DOMContentLoaded', () =>  {
+        const select = document.getElementById('items');
+        const img = document.getElementById('displayImage');
+        const fotografo = document.getElementById('photographer');
+        const description = document.getElementById('description');
+        const valor = document.getElementById('score');
+        const buttonUp = document.getElementById('increaseScore');
+        const buttonDown = document.getElementById('decreaseScore');
+
+        // Llenar select
+        for (let key in itemData) {
+            const option = document.createElement('option');
+            option.value = key;
+            option.textContent = itemData[key].name;
+            select.appendChild(option);
+        }
+
+        // Mostrar datos
+        function show(key) {
+            const item = itemData[key];
+            if (!item) return;
+            img.src = item.image;
+            fotografo.value = item.photographer;
+            description.value = item.description;
+            valor.value = item.score;
+        }
+
+        // Mostrar el primer item al cargar (simple y directo)
+        select.selectedIndex = 1; // Selecciona el primer item real (índice 0 es "Seleccione un valor")
+        show(select.value);
+
+    // Al cambiar de item
+        select.addEventListener('change', (e) => {
+                show(select.value);
+            });
+
+
+        // Aumentar score
+        buttonUp.addEventListener('click', () => {
+            const key = select.value;
+            if (itemData[key]) {
+                itemData[key].score++;
+                valor.value = itemData[key].score;
+            }
+        });
+
+        // Disminuir score
+        buttonDown.addEventListener('click', () => {
+            const key = select.value;
+            if (itemData[key]) {
+                itemData[key].score--;
+                valor.value = itemData[key].score;
+            }
+        });
+    });
+})();
+
